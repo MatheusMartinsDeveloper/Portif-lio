4cbb285b177578c351a8b4980fbe2f01fbae157a
MatheusMartinsDeveloper <matheus2005brr@gmail.com>
2024-09-30 15:53:32 -0300

    feat(homepage): Building Header component

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,16 @@
+
+export default function Header() {
+    return (
+        <header>
+            <div></div>
+            <nav>
+                <ul>
+                    <li>Sobre</li>
+                    <li>Projetos</li>
+                    <li>Habilidades</li>
+                    <li>Contato</li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
