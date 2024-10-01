a6b42f1edab578497a489402a485ceafb680509c
MatheusMartinsDeveloper <matheus2005brr@gmail.com>
2024-10-01 16:44:08 -0300

    feat(homepage): Building Section Hero and add styles

--- src/app/components/SectionHero.tsx
+++ src/app/components/SectionHero.tsx
@@ -1,6 +1,16 @@
+import { MdOutlineFileDownload } from "react-icons/md";
+import { FaArrowUp } from "react-icons/fa";
 
 export default function SectionHero() {
     return (
-        <section></section>
+        <section className="section-hero">
+            <span>Olá Mundo - essa é a minha página de portifólio</span>
+            <h1>Meu  nome é <span>Matheus</span> <br /> Eu sou Desenvolvedor FullStack Jr</h1>
+            <p>Mesclar os melhores elementos de frontend e backend para forjar <br /> experiências inovadoras para o usuário.</p>
+            <div>
+                <button><span>Download CV <MdOutlineFileDownload /></span></button>
+                <button>Contato <FaArrowUp /></button>
+            </div>
+        </section>
     );
 }
